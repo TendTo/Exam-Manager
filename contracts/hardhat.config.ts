5d00f032610e6aa216847b44cc698d7c1130399e
simo <codesimo@gmail.com>
2022-07-09 16:56:30 +0200

    chore: hardhat bullshits

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,14 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY ?? "ALKEMY API KEY";
-const ROPSTEN_PK = process.env.ROPSTEN_PK ?? "YOUR ROPSTEN PRIVATE KEY";
+const ROPSTEN_PK = process.env.ROPSTEN_PK ?? "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
+    
     ropsten: {
       url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [ROPSTEN_PK],
+      accounts: ROPSTEN_PK ? [ROPSTEN_PK] : [],
     },
   },
 };
