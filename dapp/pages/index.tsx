42aa516a46a98c2985805ad4673c4a9dc373f578
simo <codesimo@gmail.com>
2022-07-16 17:46:34 +0200

    feat: add types to dapp

--- dapp/pages/index.tsx
+++ dapp/pages/index.tsx
@@ -1,11 +1,14 @@
-import { useEtherBalance, useEthers } from "@usedapp/core";
+import { useEtherBalance, useEthers, useLogs } from "@usedapp/core";
 import { useThemeContext } from "context";
 import type { NextPage } from "next";
 import Link from "next/link";
+import { ExamContract__factory, ExamContract } from "types";
 
 const Home: NextPage = () => {
-  const { account, activateBrowserWallet } = useEthers();
+  const { account, activateBrowserWallet, library } = useEthers();
   const balance = useEtherBalance(account);
+  const contract = library ? ExamContract__factory.connect("indirizzo sulla blockchain", library) : undefined;
+  const logs = useLogs(contract ? {contract, event: "TestPassed", args: []}: undefined);
   const { state } = useThemeContext();
 
   return (
@@ -13,6 +16,7 @@ const Home: NextPage = () => {
       <div>{balance ? balance.toString() : "No banane"}</div>
       <button onClick={activateBrowserWallet}>Login</button>
       <br />
+      <dig>{logs?.value.}</dig>
       <div>{state}</div>
       <Link href='/students'>
         <a>Go student</a>
