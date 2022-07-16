8c63c18054c08c1477e8af77ac6af4286b9c55a0
simo <codesimo@gmail.com>
2022-07-16 19:39:20 +0200

    Idk: roba Co-authored-by: Tend <TendTo@users.noreply.github.com>

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -7,10 +7,14 @@ const ROPSTEN_PK = process.env.ROPSTEN_PK ?? "";
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
-    
     ropsten: {
       url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: ROPSTEN_PK ? [ROPSTEN_PK] : [],
+    },
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      accounts: ["9c7a6375a3acf88b8f9cee2ccb039b7f5baa9ba86b4b39b9208062c7c92a3b99"],
+      chainId: 1337,
     },
   },
 };
