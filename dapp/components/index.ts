e4af3ccc67a59f51cd0863db44dbf68e2d3c9def
simo <codesimo@gmail.com>
2022-07-17 19:46:20 +0200

    feat: add logos
    Co-authored-by: Tend <TendTo@users.noreply.github.com>

--- dapp/components/index.ts
+++ dapp/components/index.ts
@@ -1,1 +1,6 @@
 export { default as Layout } from "./layout";
+export { default as Header } from "./header";
+export { default as ThemeSwitcher } from "./themeSwitcher";
+export { default as LogoUnict } from "./logoUnict";
+export { default as LogoGitHub } from "./logoGitHub";
+export { default as LogoMetamask } from "./logoMetamask";
