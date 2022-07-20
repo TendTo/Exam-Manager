b8dc43dd2bd62163af45cb0387ef11d8d592d739
Ernesto Casablanca <casablancaernesto@gmail.com>
2022-07-21 00:20:07 +0200

    fix(dapp): header css

--- dapp/components/header.tsx
+++ dapp/components/header.tsx
@@ -12,9 +12,9 @@ export default function Header() {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
