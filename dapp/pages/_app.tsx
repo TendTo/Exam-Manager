c5b6fdc2b4175288732a649531d6713f5cc4be78
simo <codesimo@gmail.com>
2022-07-25 20:16:59 +0200

    fix: scroll wheel on input number
    Co-authored-by: Tend <TendTo@users.noreply.github.com>

--- dapp/pages/_app.tsx
+++ dapp/pages/_app.tsx
@@ -2,8 +2,18 @@ import "styles/globals.css";
 import type { AppProps } from "next/app";
 import { ContextProvider } from "context";
 import { Layout, LoginGuard, SetSubjectTestsModal } from "components";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
+  useEffect(() => {
+    document.addEventListener("wheel", function (event) {
+      if (document?.activeElement?.matches("[type='number']")) {
+        console.log("blocked");
+        (event.target as HTMLInputElement).blur();
+      }
+    });
+  }, []);
+
   return (
     <ContextProvider>
       <LoginGuard router={router} />
