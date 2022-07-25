9d8e5b8daf939b942bae59fed6bd6fe422766384
simo <codesimo@gmail.com>
2022-07-26 01:11:00 +0200

    feat: init student front-end

--- dapp/components/pendingSubject.tsx
+++ dapp/components/pendingSubject.tsx
@@ -5,9 +5,10 @@ type PendingSubjectProps = {
   library: JsonRpcProvider | undefined;
   mark: number;
   subjectId: number;
+  tests: Record<string, { mark: number; passed: boolean }>;
 };
 
-export default function PendingSubject({ library, mark, subjectId }: PendingSubjectProps) {
+export default function PendingSubject({ library, mark, subjectId, tests }: PendingSubjectProps) {
   const { value: subjectInfo } = useSubjects(library, subjectId);
 
   if (!subjectInfo) return <></>;
@@ -17,6 +18,15 @@ export default function PendingSubject({ library, mark, subjectId }: PendingSubj
         <div>{subjectInfo.name}</div>
         <div>CFU: {subjectInfo.cfu}</div>
         <div>{mark}</div>
+        {Object.entries(tests).map(([testId, test]) => (
+          <div key={testId}>
+            TestId: {testId}
+            <br />
+            {test.passed ? "✅" : "❌"}
+            <br />
+            Voto {test.mark}
+          </div>
+        ))}
       </td>
     </tr>
   );
