209f7056798f44d154eb8cae765acfcf2fc3e799
Ernesto Casablanca <casablancaernesto@gmail.com>
2022-07-28 09:43:56 +0200

    style(dapp): fix footer

--- dapp/components/footer.tsx
+++ dapp/components/footer.tsx
@@ -2,14 +2,13 @@ import { LogoUnict, LogoGitHub } from "components";
 
 export default function Footer() {
   return (
-    <footer className="footer items-center p-4 bg-neutral text-neutral-content sm:grid-flow-col">
+    <footer className="footer items-center p-4 bg-neutral text-neutral-content grid-flow-col">
       <div className="grid-flow-col">
         <a href="https://github.com/TendTo/Exam-Manager">
           <LogoUnict />
         </a>
-        <p>Copyright © 2022 - All right reserved</p>
       </div>
-      <div className="grid-flow-col gap-4 sm:justify-self-end">
+      <div className="grid-flow-col gap-4 justify-self-end">
         <a href="https://github.com/TendTo/Exam-Manager">
           <LogoGitHub />
         </a>
